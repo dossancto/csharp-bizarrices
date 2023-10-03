249bfe19d734246eb1f46dc8ef401979e6d8305f
lu-css <dossantossilvalucas963@gmail.com>
2023-10-03 13:18:36 -0300

    add mathing project

---
+++ Mathing/Calc.cs
@@ -0,0 +1,130 @@
+public abstract class Calculator
+{
+    public static Calculator New(int number)
+    {
+        if (number == 0)
+            return new Zero();
+
+        return number > 0 ? new Positive(number) : new Negative(number);
+    }
+
+    public static bool operator true(Calculator c) => !(c.isZero);
+    public static bool operator false(Calculator c) => c.isZero;
+    public static bool operator !(Calculator c) => c ? false : true;
+    public static explicit operator int(Calculator c) => c.Number;
+    public static implicit operator Calculator(int number) => Calculator.New(number);
+
+    public double Factorial
+    {
+        get
+        {
+            if (!this) return 0;
+            if (Number.Abs() == 1) return Number;
+
+            var result = Fac(Number);
+
+            return isNegative ? -result : result;
+        }
+    }
+
+    private double Fac(int n)
+    {
+        n = n.Abs();
+
+        if (n == 1) return n;
+
+        return n * (Fac(n - 1));
+    }
+
+    public double Pow(int n) =>
+      Math.Pow(Number, n);
+
+    public string Sqrt
+    {
+        get => MatchSqrt(
+            pos => Math.Sqrt(pos).ToString(),
+            neg => $"{Math.Sqrt(neg.Abs())}i"
+            );
+    }
+
+
+    public abstract R Match<R>(Func<int, R> positiveFunc, Func<int, R> negativeFunc, Func<R> zeroFunc);
+    public abstract R MatchSqrt<R>(Func<int, R> positiveFunc, Func<int, R> negativeFunc);
+
+    public int AbsoluteNumber
+    {
+        get => Match(
+            p => p,
+            n => -n,
+            () => 0
+          );
+    }
+
+    public int Number
+    {
+        get => Match(
+            p => p,
+            n => n,
+            () => 0
+          );
+    }
+
+    public bool isZero
+    {
+        get => Match(
+          _ => false,
+          _ => false,
+          () => true
+        );
+    }
+
+    public bool isPositive
+    {
+        get => Match(
+          positive => true,
+          negative => false,
+          () => false
+        );
+    }
+
+    public bool isNegative
+    {
+        get => !isZero && !isPositive;
+    }
+
+    public sealed class Zero : Calculator
+    {
+        public override R Match<R>(Func<int, R> positiveFunc, Func<int, R> negativeFunc, Func<R> zeroFunc) =>
+             zeroFunc();
+
+        public override R MatchSqrt<R>(Func<int, R> positiveFunc, Func<int, R> negativeFunc) => positiveFunc(0);
+    }
+    public sealed class Positive : Calculator
+    {
+        private readonly int Value;
+
+        public Positive(int value) =>
+            Value = value;
+
+        public override R Match<R>(Func<int, R> positiveFunc, Func<int, R> negativeFunc, Func<R> zeroFunc) =>
+          positiveFunc(Value);
+
+        public override R MatchSqrt<R>(Func<int, R> positiveFunc, Func<int, R> negativeFunc) =>
+          positiveFunc(Value);
+    }
+
+    public class Negative : Calculator
+    {
+        private readonly int Value;
+
+        public Negative(int value) =>
+            Value = value;
+
+
+        public override R Match<R>(Func<int, R> positiveFunc, Func<int, R> negativeFunc, Func<R> zeroFunc) =>
+          negativeFunc(Value);
+
+        public override R MatchSqrt<R>(Func<int, R> positiveFunc, Func<int, R> negativeFunc) =>
+          negativeFunc(Value);
+    }
+}
---
+++ Mathing/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Calculator cal = -4;
+
+        Console.WriteLine($"O número é {(int)cal}");
+
+        if (cal.isZero) Console.WriteLine("O número é ZERO meu mano");
+
+        if (cal.isPositive) Console.WriteLine("O número é positivo meu brother");
+
+        if (!cal.isPositive) Console.WriteLine("O número é negativo meu heroi");
+
+        var msg = cal.Match(
+           p => "Aqui é ativado se for positivo",
+           n => "Aqui é ativado se for negativo",
+           () => "Aqui é ativado se for zero"
+       );
+
+        Console.WriteLine(msg);
+
+        var factorial = cal.Factorial;
+        Console.WriteLine($"Factorial: {factorial}");
+
+        Console.WriteLine($"Pow: {cal.Pow(3)}");
+
+        Console.WriteLine($"Raiz quadrada: {cal.Sqrt}");
+    }
+}
